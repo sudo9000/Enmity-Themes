dc828a795851a9a5cc78e05d07ecd27d0bdab419
sudo <sudo9000@outlook.com>
2022-06-05 21:55:56 -0700

    quick add links

---
+++ merlot.php
@@ -0,0 +1,4 @@
+<?php
+  header('com.hammerandchisel.discord://enmity?id=-1&command=install-theme&params=https://raw.githubusercontent.com/sudo9000/Enmity-Themes/main/Merlot.json', true, 301); 
+  exit;
+?>
