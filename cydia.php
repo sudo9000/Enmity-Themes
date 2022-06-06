404eed70a9453d09b97995d988b33b03c549facf
sudo <sudo9000@outlook.com>
2022-06-05 21:59:49 -0700

    .

--- cydia.php
+++
@@ -1,4 +0,0 @@
-<?php
-  header('com.hammerandchisel.discord://enmity?id=-1&command=install-theme&params=https://raw.githubusercontent.com/sudo9000/Enmity-Themes/main/Cydia.json', true, 301); 
-  exit;
-?>
